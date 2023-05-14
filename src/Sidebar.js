751c188c6f9263be50ff5e6e729ca84645a3e9c2
StevenWeng60 <stevenweng60@gmail.com>
2023-05-14 16:07:59 -0500

    Commit before adding font-awesome icons

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,6 +3,26 @@ import "./Sidebar.css"
 function Sidebar() {
   return (
     <div className="Sidebar">
+      <div className="websiteTitle">
+        <h1>Warped</h1>
+      </div>
+      <ul className="sidebarOptions">
+        <li>
+          <h2>Profile</h2>
+        </li>
+        <li>
+          <h2>Home</h2>
+        </li>
+        <li>
+          <h2>Messages</h2>
+        </li>
+        <li>
+          <h2>Post</h2>
+        </li>
+        <li>
+          <h2>Warp</h2>
+        </li>
+      </ul>
       <h1>Sidebar here bro</h1>
     </div>
   );
