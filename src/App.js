2860c917c2fa4847f9dc0e26efabbf83543b20aa
StevenWeng60 <stevenweng60@gmail.com>
2023-05-15 12:10:36 -0500

    Added temporary friendsicon list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Sidebar from './Sidebar.js';
 import Searchbar from './Searchbar.js';
+import FriendIcons from './FriendIcons.js';
 
 function App() {
   return (
@@ -12,8 +13,10 @@ function App() {
         <div className="sidebar">
           <Sidebar/>
         </div>
+        <div className="friendicons">
+          <FriendIcons/>
+        </div>
         <div className="mainfeed">
-          adsfklj;
         </div>
       </div>
     </div>
---
+++ src/FriendIcons.js
@@ -0,0 +1,17 @@
+import './FriendIcons.css'
+import { people } from './data.js';
+import { getImageUrl } from './utils.js';
+
+function FriendIcons() {
+  const listItems = people.map(person =>
+    <li>
+      <img
+        src={getImageUrl(person)}
+        alt={person.name}
+      />
+    </li>
+  );
+  return <ul className="friendsul">{listItems}</ul>;
+}
+
+export default FriendIcons;
