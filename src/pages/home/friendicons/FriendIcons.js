9ece69dd02b8ca1c4c00985612e4e709e1d200b3
StevenWeng60 <stevenweng60@gmail.com>
2023-05-27 13:52:52 -0500

    Properly loaded friends profile icon in home page

--- src/pages/home/friendicons/FriendIcons.js
+++ src/pages/home/friendicons/FriendIcons.js
@@ -1,33 +1,61 @@
 import './FriendIcons.css'
 import { people } from '../../../utilities/data.js';
-import { getImageUrl } from '../../../utilities/utils.js';
+import { useEffect, useState } from 'react'
+import { Buffer } from 'buffer'
 import axios from 'axios';
 
 function FriendIcons() {
-  let friends = []
+  const [listOfFriendsInfo, setListOfFriendsInfo] = useState([]);
+
+  useEffect(() => {
+    getFriends();
+  }, []);
+
   function getFriends() {
     axios.post("http://localhost:3001/friendicons",{
       username: localStorage.getItem("Username")
     })
     .then(function (response) {
-      console.log(response);
-      friends = response.data.friends;
-      console.log(friends);
+      console.log(response.data.friends);
+      const friends = response.data.friends;
+      
+      const friendsInfo = friends.map((friend) => {
+        const dataUrl = `data:${friend['avatarContentType']};base64,${Buffer.from(friend.avatar, 'binary').toString('base64')}`;
+        return {
+          username: friend.username,
+          imageUrl: dataUrl
+        }
+      });
+      
+      setListOfFriendsInfo(friendsInfo);
     })
     .catch((error) =>{
       console.log(error);
     })
   }
-  getFriends();
-  const listItems = friends.map(friend =>
-    <li key={friend.id}>
-      <img
-        src={getImageUrl(friend.username)}
-        alt={friend.username}
-      />
-    </li>
+  // const listItems = friends.map(friend =>
+  //   <li key={friend.id}>
+  //     <img
+  //       src={getImageUrl(friend.username)}
+  //       alt={friend.username}
+  //     />
+  //   </li>
+  // );
+  return (
+  <ul className="friendsul">
+    {listOfFriendsInfo.map((friend) => {
+      return (
+        <li key={friend._id}>
+          <img 
+            src={friend.imageUrl}
+            alt={friend.username}
+          />
+          <p>{friend.username}</p>
+        </li>
+      )
+    })}
+  </ul>
   );
-  return <ul className="friendsul">{listItems}</ul>;
 }
 
 export default FriendIcons;
