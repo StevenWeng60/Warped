fa55616187674589b5f35d7dac58b98d55c99f68
StevenWeng60 <stevenweng60@gmail.com>
2023-06-02 16:36:25 -0500

    Made sidebar reponsive

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,68 +1,91 @@
 import "./Sidebar.css"
-import {React, useRef} from 'react';
+import {React, useRef, useState, useEffect} from 'react';
 import { FaHome, FaUserCircle, FaMailBulk, FaRegPlusSquare, FaToriiGate, FaSearch } from "react-icons/fa";
 import { Link, Route, Routes } from "react-router-dom"
 import axios from 'axios';
+import IconSidebar from "./iconsidebar/IconSidebar";
 
 function Sidebar() {
+  // Get viewport width
+  const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    function handleResize() {
+      setViewportWidth(window.innerWidth)
+    }
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
   const profileRoute = '/profile/' + localStorage.getItem("Username") + "/me";
 
   return (
-    <div className="Sidebar">
-      <div className="websiteTitle">
-        <h1>Warped</h1>
+    <div>
+      {viewportWidth > 768
+      ? <div className="Sidebar">
+        <div className="websiteTitle">
+          <h1>Warped</h1>
+        </div>
+        <ul className="sidebarOptions">
+          <li>
+              <div className="sideIcon">
+                <Link to={profileRoute} className="link">
+                <FaHome className="iconStyles"/>
+                <h4 className="lih2">Profile</h4>
+                </Link>
+              </div>
+          </li>
+          <li>
+              <div className="sideIcon">
+                <Link to="/" className="link">
+                <FaUserCircle className="iconStyles"/>
+                <h4 className="lih2">Home</h4>
+                </Link>
+              </div>
+          </li>
+          <li>
+              <div className="sideIcon">
+                <Link to="/messages" className="link">          
+                <FaMailBulk className="iconStyles"/>
+                <h4 className="lih2">Message</h4>
+                </Link>
+              </div>
+          </li>
+          <li>
+            <div className="sideIcon">
+              <Link to="/post" className="link">
+                <FaRegPlusSquare className="iconStyles"/>
+                <h4 className="lih2">Post</h4>
+              </Link>
+            </div>
+          </li>
+          <li>
+            <div className="sideIcon">
+              <Link to="/search" className="link">
+                <FaSearch className="iconStyles"/>
+                <h4 className="lih2">Search</h4>
+              </Link>
+            </div>
+          </li>
+          <li>
+            <div className="sideIcon">
+              <Link to="/search" className="link">
+              <FaToriiGate className="iconStyles"/>
+              <h4 className="lih2">Warp</h4>
+              </Link>
+            </div>
+          </li>
+          <li className="logoutbtn">
+            <h4>{localStorage.getItem("Username")}</h4>
+            <h4>Log out</h4>
+          </li>
+        </ul>
       </div>
-      <ul className="sidebarOptions">
-        <li>
-          <Link to={profileRoute} className="link">
-            <div>
-              <FaHome style={{fontSize: '2em'}}/>
-              <h2 className="lih2">  Profile</h2>
-            </div>
-          </Link>
-        </li>
-        <li>
-          <Link to="/" className="link">
-            <div>
-              <FaUserCircle style={{fontSize: '2em'}}/>
-              <h2 className="lih2">  Home</h2>
-            </div>
-          </Link>
-        </li>
-        <li>
-          <Link to="/messages" className="link">          
-            <div>
-              <FaMailBulk style={{fontSize: '2em'}}/>
-              <h2 className="lih2">  Messages</h2>
-            </div>
-          </Link>
-        </li>
-        <li>
-          <div>
-            <Link to="/post" className="link">
-              <FaRegPlusSquare style={{fontSize: '2em'}}/>
-              <h2 className="lih2">  Post</h2>
-            </Link>
-          </div>
-        </li>
-        <li>
-          <div>
-            <Link to="/search" className="link">
-              <FaSearch style={{fontSize: '2em'}}/>
-              <h2 className="lih2">  Search</h2>
-            </Link>
-          </div>
-        </li>
-        <li>
-          <div>
-            <FaToriiGate style={{fontSize: '2em'}}/>
-            <h2 className="lih2">  Warp</h2>
-          </div>
-        </li>
-        <li>
-          <h2>{localStorage.getItem("Username")}</h2>
-        </li>
-      </ul>
+      :
+      <IconSidebar/>
+      }
     </div>
   );
 }
