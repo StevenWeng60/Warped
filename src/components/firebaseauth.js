6e54ed249cf3b08251e6241a5d21313b760031b0
StevenWeng60 <stevenweng60@gmail.com>
2023-06-26 19:58:49 -0500

    Changed firebase authentication to allow persistent user authentication across page refreshes

--- src/components/firebaseauth.js
+++ src/components/firebaseauth.js
@@ -1,27 +1,51 @@
 import { useNavigate } from 'react-router-dom'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { getAuth } from 'firebase/auth'
 import { auth } from '../config/firebase-config'
 
 const firebaseAuth = (Component) => {
   const AuthenticateComponent = () => {
     const navigate = useNavigate();
-  
+    const [loading, setLoading] = useState(true);
+
     useEffect(() => {
-      // Check to see if user is authenticated
-      // if they are, allow them to see the home page
-      // if not, redirect them to the login page
-      const user = auth.currentUser;
-      
-      if(!user) {
-        navigate('/login');
-      }
-  
+      const checkAuthentication = async () => {
+        try {
+          const user = await new Promise((resolve, reject) => {
+            // Subscribe to the auth state changes and wait for initial check
+            const unsubscribe = auth.onAuthStateChanged((user) => {
+              unsubscribe(); // Unsubscribe once the initial check is done
+              resolve(user);
+            }, reject);
+          });
+
+          if (user) {
+            // User is authenticated, allow them to see the component
+            setLoading(false);
+          } else {
+            // User is not authenticated, redirect them to the login page
+            navigate('/login');
+          }
+        } catch (error) {
+          console.error('Error checking authentication:', error);
+          // Handle the error accordingly (e.g., show an error message)
+        }
+      };
+
+      checkAuthentication();
     }, [navigate]);
-    
-    return <Component/>
-  }
-  
+
+    // Render a loading state while the authentication state is being checked
+    if (loading) {
+      return (<div className="App">
+        <div className="Body" style={{background: "#393646"}}>
+        </div>
+      </div>);
+    }
+
+    return <Component />;
+  };
+
   return AuthenticateComponent;
 }
 
--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, setPersistence, browserLocalPersistence } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage, ref } from 'firebase/storage';
 // TODO: Add SDKs for Firebase products that you want to use
@@ -22,7 +22,17 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
+const auth = getAuth(app);
+
+setPersistence(auth, browserLocalPersistence)
+.then(() => {
+  console.log('Persistence set successfully');
+})
+.catch((error) => {
+  console.error(error);
+})
+
+export { auth }
 
 export const googleProvider = new GoogleAuthProvider();
 
